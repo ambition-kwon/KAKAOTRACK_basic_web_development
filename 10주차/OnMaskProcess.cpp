7a6efb0dac5be40e60c3f03b73f14e76d55ddd95
ambition-kwon <prince5390@gmail.com>
2022-11-15 20:05:12 +0900

    10-2

---
+++ 10주차/OnMaskProcess.cpp
@@ -0,0 +1,29 @@
+void CImageProcessing2021108030Doc::OnNearest()
+{
+	int i, j;
+	int ZoomRate = 2;
+	double** tempArray;
+
+	m_Re_height = int(ZoomRate * m_height);
+	m_Re_width = int(ZoomRate * m_width);
+	m_Re_size = m_Re_height * m_Re_width;
+	m_tempImage = Image2DMem(m_height, m_width);
+	tempArray = Image2DMem(m_Re_height, m_Re_width);
+	m_OutputImage = new unsigned char[m_Re_size];
+
+	for (i = 0; i < m_height; i++) {
+		for (j = 0; j < m_width; j++) {
+			m_tempImage[i][j] = (double)m_InputImage[i * m_width + j];
+		}
+	}
+	for (i = 0; i < m_Re_height; i++) {
+		for (j = 0; j < m_Re_width; j++) {
+			tempArray[i][j] = m_tempImage[i / ZoomRate][j / ZoomRate];
+		}
+	}
+	for (i = 0; i < m_Re_height; i++) {
+		for (j = 0; j < m_Re_width; j++) {
+			m_OutputImage[i * m_Re_width + j] = (unsigned char)tempArray[i][j];
+		}
+	}
+}

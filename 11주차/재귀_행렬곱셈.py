c62f2b520eaaaba5f4a4ac694614929eddf8e9bc
ambition-kwon <prince5390@gmail.com>
2022-11-21 22:55:52 +0900

    11-1

---
+++ 11주차/재귀_행렬곱셈.py
@@ -0,0 +1,41 @@
+import math
+
+count = 0
+tot = int(input("계산할 행렬의 갯수를 입력하시오: "))
+matrix = [[] for i in range(tot+1)] # index를 1부터 사용할 것이기 때문에 tot + 1
+dp = [[None] * (tot+1) for _ in range(tot+1)] # 각 부분문제 결과를 저장 하기위한 배열
+matrix[0] = [0, 0] # 배열의 1번째 부터 사용할 것이기 때문에
+for i in range(tot):
+    row = int(input(f"{i+1}번째 행렬의 행(row)값을 입력하시오 : "))
+    col = int(input(f"{i+1}번째 행렬의 열(col)값을 입력하시오 : "))
+    matrix[i+1].append(row)
+    matrix[i+1].append(col)
+
+def Recursive_MatrixChain(i, j):
+    global count #재귀가 반복되어도 변수 값을 유지하기 위해
+    count += 1
+    if i == j: 
+        dp[i][j] = 0
+        return 0 # 재귀 종료조건
+    min = math.inf # 가장 큰 수
+    for k in range(i, j): # i ~ j-1까지
+        temp = Recursive_MatrixChain(i, k) + Recursive_MatrixChain(k+1, j) + (matrix[i][0] * matrix[k][1] * matrix[j][1])
+        if temp < min: 
+            min = temp
+            dp[i][j] = temp
+    return min
+
+###### 최소값을 출력 #######
+print(Recursive_MatrixChain(1, tot))
+###### 최소값을 출력 #######
+
+### 각 부분문제에 대한 결과를 출력 ###
+for row in range(1, len(dp)):
+    for col in range(1, len(dp[0])):
+        print(dp[row][col], end = " ")
+    print("\n")
+### 각 부분문제에 대한 결과를 출력 ###
+
+### 재귀 호출 횟수를 출력 ###
+print(f"함수 재귀 호출 횟수 : {count}번")
+### 재귀 호출 횟수를 출력 ###
